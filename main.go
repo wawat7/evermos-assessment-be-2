0a1f2f600b5c8e140eefba6eb491ce30c0cf3616
Wawat Prigala <wawatprigala00@gmail.com>
2022-02-03 11:33:59 +0700

    Treasure Hunt - create command move postition - create dollar if get the treasure

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 type Position struct {
@@ -11,34 +10,68 @@ type Position struct {
 }
 
 func main() {
-	fmt.Println("asd")
+
+	position := Position{X: 2, Y: 2}
+	_, _ = runApp(position)
+
+	tempPosition := Position{X: position.X, Y: position.Y}
+
+	callTyping(position, tempPosition)
+}
+
+func callTyping(position Position, temp Position) {
+	typing := ""
+
+	temp.X = position.X
+	temp.Y = position.Y
+
+	fmt.Print("type up/down/right/left ?")
+	fmt.Scan(&typing)
+
+	switch typing {
+	case "up":
+		position.X += 1
+	case "down":
+		position.X -= 1
+	case "right":
+		position.Y += 1
+	case "left":
+		position.Y -= 1
+	}
+
+	success, warning := runApp(position)
+
+	if !success && warning {
+		_, _ = runApp(temp)
+		callTyping(temp, temp)
+	} else if !success && !warning {
+		callTyping(position, temp)
+	} else {
+		fmt.Println("game finished")
+	}
+}
+
+func runApp(position Position) (success bool, warning bool) {
+	fmt.Println("")
+	var pagar []Position
+
+	treasure := Position{X: 5, Y: 6}
 
 	n := 9
 	row := 7
 
-	position := Position{
-		X: 2,
-		Y: 3,
-	}
-
-	beforePosition := Position{
-		X: 2,
-		Y: 2,
-	}
-
-	alert := ""
-
 	for i := row; i >= 0; i-- {
-		fmt.Print(i)
+		fmt.Print("")
 		k := 0
 		for j := 1; j <= n; j++ {
 			k = k + 1
 			if i == 0 || i == row {
-				fmt.Print(k)
+				fmt.Print("")
 			} else {
 				print := "#"
 				if j == n {
-					print = strconv.Itoa(k)
+					// print = strconv.Itoa(k)
+					print = ""
 				} else {
 
 					if i == 2 && (j == 2 || j == 4 || j == 5 || j == 6 || j == 7) {
@@ -57,20 +90,16 @@ func main() {
 						print = "."
 					}
 
-					// fmt.Print("------")
-					// fmt.Print(print)
-					// fmt.Print("------")
-
 					if print == "#" {
-						alert = "tidak bisa"
-						beforePosition.X = position.X
-						beforePosition.Y = position.Y
+						pagar = append(pagar, Position{X: i, Y: j})
 
-					} else {
-						alert = ""
 					}
 					if position.X == i && position.Y == j {
-						print = "X"
+						if position.X == treasure.X && position.Y == treasure.Y {
+							print = "$"
+						} else {
+							print = "X"
+						}
 					}
 
 				}
@@ -82,6 +111,22 @@ func main() {
 		fmt.Println("")
 	}
 
+	alert := ""
+	for _, pg := range pagar {
+		if pg.X == position.X && pg.Y == position.Y {
+			alert = "FORBIDDEN"
+		}
+	}
+
 	fmt.Println(alert)
 
+	if alert != "" {
+		return false, true
+	}
+
+	if position.X == treasure.X && position.Y == treasure.Y {
+		return true, false
+	}
+
+	return false, false
 }
